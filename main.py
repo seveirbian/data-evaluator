6d652e1d49ddec56a62b84ebbca89a6ae6f9f87c
seveirbian <1031180278@qq.com>
2026-05-25 11:59:37 +0800

    feat: update main.py to use MultiScalingCurvePlotter

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from src.scaling_curve_evaluator import ScalingCurveGenerator
+from src.scaling_curve_evaluator import MultiScalingCurvePlotter
 
 
 def main():
-    gen = ScalingCurveGenerator(
-        policy_dir="example/policy/act_policy_grabcuicuishaplacematting-30",
-        train_data_dir="example/data/train/Task-GrabCuicuishaPlaceMatting-30",
+    plotter = MultiScalingCurvePlotter(
         eval_data_dir="example/data/eval/Task-GrabCuicuishaPlaceMatting-30",
-        hook_module="model.backbone",
+        curves=[
+            {
+                "policy_dir": "example/policy/act_policy_grabcuicuishaplacematting-30",
+                "train_data_dir": "example/data/train/Task-GrabCuicuishaPlaceMatting-30",
+                "hook_module": "model.backbone",
+            },
+        ],
         device="auto",
         num_points=10,
     )
-    gen.generate()
-    gen.plot(save_path="scaling_curve.png", show=True)
+    plotter.generate_all()
+    plotter.plot(save_path="scaling_curve.png", show=False)
     print("Done. Saved to scaling_curve.png")
 
 
