5b01f81e3374d0ce4f5413df1a83c9ca7c22b87a
seveirbian <1031180278@qq.com>
2026-06-04 16:45:35 +0800

    [feat] add .vscode

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from src.scaling_curve import MultiScalingCurveGenerator
 
 def main():
     plotter = MultiScalingCurveGenerator(
-        eval_data_dir="example/data/eval/Task-GrabCuicuishaPlaceMatting-30",
+        eval_data_dir="example/cuicuisha/data/eval/eval_act_Task-GrabCuicuishaPlaceMatting-30",
         curves=[
             {
-                "policy_dir": "example/policy/act_policy_grabcuicuishaplacematting-30",
-                "train_data_dir": "example/data/train/Task-GrabCuicuishaPlaceMatting-30",
+                "policy_dir": "example/cuicuisha/policy/act_policy_grabcuicuishaplacematting-30",
+                "train_data_dir": "example/cuicuisha/data/train/Task-GrabCuicuishaPlaceMatting-30",
                 "hook_module": "model.backbone",
             },
         ],
         device="auto",
-        num_points=10,
+        num_points=5,
     )
     plotter.generate_all()
-    plotter.plot(save_path="scaling_curve.png", show=False)
+    plotter.plot(save_path="cuicuisha-30.png", show=True)
     print("Done. Saved to scaling_curve.png")
 
 
