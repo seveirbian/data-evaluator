b85295b2d73c2e4610f78505b54e574fbf3167ec
seveirbian <1031180278@qq.com>
2026-06-05 13:18:57 +0800

    feat: export OpenPIEmbeddingExtractor from package

--- src/scaling_curve/__init__.py
+++ src/scaling_curve/__init__.py
@@ -1,6 +1,8 @@
+from ._embeddings_openpi import OpenPIEmbeddingExtractor
 from .scaling_curve import MultiScalingCurveGenerator, ScalingCurveGenerator
 
 __all__ = [
+    "OpenPIEmbeddingExtractor",
     "ScalingCurveGenerator",
     "MultiScalingCurveGenerator",
 ]
